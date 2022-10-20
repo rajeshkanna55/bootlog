3c79cb206403b2761d9cc948b5422d4dcad9d9f0
rajeshkanna55 <rkathiravan889@gmail.com>
2022-10-20 10:59:11 +0530

    chenges cod

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function submit(){
+
+    // var userName=document.getElementById("mail").value;
+    // var passWord=document.getElementById("pass").value;
+    console.log("hi");
+//    if(userName=='' && passWord==''){
+//         userName.style.bordercolor="red";;
+
+//     }
+}   
